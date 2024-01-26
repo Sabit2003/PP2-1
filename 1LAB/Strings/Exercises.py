130264b15b1e2e4e615110091c9d46562386d6ca
Ganishka04 <157820717+Ganishka04@users.noreply.github.com>
2024-01-26 21:49:14 +0600

    Add files via upload

---
+++ 1LAB/Strings/Exercises.py
@@ -0,0 +1,14 @@
+x = "Hello World"
+print(x)
+
+txt = "Hello World"
+x = txt[0]
+x = txt[2:5]
+x = txt.strip()
+x = txt.upper()
+x = txt.lower()
+x = txt.replace("H","J")
+
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))
